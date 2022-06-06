8cc96849bfd0cdaaa8b1ac906ce48c2fd794d5ba
PacoDiMarrakech <ayoub.boufarra@outlook.com>
2022-06-06 11:37:30 +0200

    Application with a basic presentation

---
+++ src/main/java/io/javabrains/inbox/InboxApp.java
@@ -0,0 +1,51 @@
+package io.javabrains.inbox;
+
+import java.nio.file.Path;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.javabrains.inbox.folders.Folder;
+import io.javabrains.inbox.folders.FolderRepository;
+
+@SpringBootApplication
+@RestController
+public class InboxApp {
+	
+	@Autowired FolderRepository folderRepository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(InboxApp.class, args);
+	}
+
+	@RequestMapping("/user")
+	public String user(@AuthenticationPrincipal OAuth2User principal) {
+		System.out.println(principal);
+		return principal.getAttribute("name");
+	}
+	
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
+	
+	@PostConstruct
+	public void init() {
+		folderRepository.save(new Folder("PacoDiMarrakech","inbox", "blue"));
+		folderRepository.save(new Folder("PacoDiMarrakech","sent", "green"));
+		folderRepository.save(new Folder("PacoDiMarrakech","Important", "yellow"));
+
+	}
+
+}
