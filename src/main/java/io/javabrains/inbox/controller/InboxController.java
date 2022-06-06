f3ec1dc0f67aceb2335497153a0e344c376dea20
PacoDiMarrakech <ayoub.boufarra@outlook.com>
2022-06-06 12:22:44 +0200

    Implement folder listing functionality

--- src/main/java/io/javabrains/inbox/controller/InboxController.java
+++ src/main/java/io/javabrains/inbox/controller/InboxController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import io.javabrains.inbox.folders.Folder;
 import io.javabrains.inbox.folders.FolderRepository;
+import io.javabrains.inbox.folders.FolderService;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -21,9 +22,9 @@ public class InboxController {
 	
 	
 	
-	@Autowired
-	private FolderRepository folderRepository;
-	
+	@Autowired private FolderRepository folderRepository;
+	@Autowired private FolderService folderService;
+
 	@GetMapping(value = "/")
 	public String homePage(@AuthenticationPrincipal OAuth2User principal, Model model) {
 
@@ -34,10 +35,11 @@ public class InboxController {
 
 
 		String userId = principal.getAttribute("login");
-		System.out.println(userId);
 		List<Folder> userFolders = folderRepository.findAllById(userId);
-
 		model.addAttribute("userFolders", userFolders);
+		
+		List<Folder> defaultFolders = folderService.fetchDefaultFolders(userId);
+		model.addAttribute("defaultFolders", defaultFolders);
 
 		
 		return "inbox-page";
